a1f64899b286b06b56400adb291253e2b6cb855e
Haunter1337 <mikkibus@gmail.com>
2017-01-19 13:27:56 +0100

    Added project
    Hello world

---
+++ Hello world/Hello world/Hello world.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    class Hello_world
+    {
+        public void Run()
+        {
+                Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+                
+
+        }
+    }
+}
